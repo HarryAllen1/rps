5a7de589db5443daa0fb449a9c003f5e252c0bf8
Harry Allen <66224939+HarryAllen1@users.noreply.github.com>
2025-04-22 12:14:40 -0700

    add algorithm

--- src/main.rs
+++ src/main.rs
@@ -23,13 +23,17 @@ enum Move {
     Scissors,
 }
 
+/// Represents the status of the player (i.e., if the player wins, it's Win)
 #[derive(PartialEq, Clone, Copy)]
 enum GameResult {
     Win,
     Loss,
     Tie,
 }
+#[derive(PartialEq, Clone, Copy)]
 struct GameOutcome {
+    player_move: Move,
+    computer_move: Move,
     result: GameResult,
 }
 
@@ -54,19 +58,32 @@ fn main() {
             "exit" => {
                 // calculate the percentage of games won.
                 // Ensure that if the player won no games, they won 0% of games.
-                let percent_won = calculate_percentage_won(past_games);
+                let percent_won = calculate_percentage_won(&past_games);
                 println!(
-                    "Thank you for playing! You won {:.2}% of the games.",
+                    "Thank you for playing! You won {:.2}% of the games (ignoring ties).",
                     percent_won
                 );
                 break;
             }
             "rock" | "paper" | "scissors" => {
-                let computer_move = random_move();
+                // if the computer winrate is lower than 50%, the player has probably figured out the computer's strategy, so go back to the random move strategy.
+                // Although, if only a few games have been played, its probably just a luck that the player is doing so well.
+                // (also handles the first move where there is no prior move).
+                let computer_move = if calculate_percentage_won(&past_games) < 0.5
+                    && (past_games.len() > 5 || past_games.len() == 0)
+                {
+                    random_move()
+                } else {
+                    find_best_move(&past_games[past_games.len() - 1])
+                };
                 let player_move = str_to_move(&command);
 
                 let winner = determine_winner(&player_move, &computer_move);
-                let game_outcome = GameOutcome { result: winner };
+                let game_outcome = GameOutcome {
+                    player_move,
+                    computer_move,
+                    result: winner,
+                };
 
                 past_games.push(game_outcome);
                 match winner {
@@ -143,6 +160,31 @@ fn str_to_move(str: &String) -> Move {
     }
 }
 
+/// Finds the best possible computer move based on the user's last move.
+/// If the player wins the past round, they are more likely to play the same move, so play the move which beats that.
+/// If the player loses the last round, they are more likely to play the move which beat their last move, so play the move which beats that.
+/// If it's a tie, just play a random move.
+fn find_best_move(previous_round: &GameOutcome) -> Move {
+    if previous_round.result == GameResult::Tie {
+        return random_move();
+    }
+
+    if previous_round.result == GameResult::Win {
+        return match previous_round.player_move {
+            Move::Rock => Move::Paper,
+            Move::Paper => Move::Scissors,
+            Move::Scissors => Move::Rock,
+        };
+    }
+
+    // game result is lose
+    match previous_round.player_move {
+        Move::Rock => Move::Scissors,
+        Move::Paper => Move::Rock,
+        Move::Scissors => Move::Paper,
+    }
+}
+
 /// Converts a Move enum member to a string so it can be printed nicely
 fn move_to_str(mv: &Move) -> String {
     match mv {
@@ -162,21 +204,26 @@ fn capitalize_first_letter(str: String) -> String {
     }
 }
 
-fn calculate_percentage_won(past_games: Vec<GameOutcome>) -> f32 {
-    // prevent dividing by zero
-    if past_games.len() == 0 {
-        return 0.0;
-    }
-
+fn calculate_percentage_won(past_games: &Vec<GameOutcome>) -> f32 {
     let mut past_game_win_count = 0;
+    let mut not_tie_games = 0;
 
     for game in past_games.iter() {
         if game.result == GameResult::Win {
             past_game_win_count += 1;
         }
+
+        if game.result != GameResult::Tie {
+            not_tie_games += 1;
+        }
     }
 
-    past_game_win_count as f32 / past_games.len() as f32 * 100.0
+    // prevent dividing by zero
+    if not_tie_games == 0 {
+        return 0.0;
+    }
+
+    past_game_win_count as f32 / not_tie_games as f32 * 100.0
 }
 
 fn determine_winner(player_move: &Move, computer_move: &Move) -> GameResult {
